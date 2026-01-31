426e1ca121c837014bdc614a1533741f58db7256
Paul <paul@mylocalhost.com>
2026-01-31 11:53:29 +1100

    chore: release 0.1.0 - first minor release (#34)
    Co-authored-by: Paul Radford <paul+git@mylocalhost.com>

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@
  */
 
 export {
+  clearBunaryConfig,
   createConfig,
   defineConfig,
   getBunaryConfig,
-  clearBunaryConfig,
 } from "./config";
 export { Environment, type EnvironmentType } from "./constants";
 export { env, isDev, isProd, isTest } from "./environment";
-export type { BunaryConfig, AppConfig, OrmConfig } from "./types";
+export type { AppConfig, BunaryConfig, OrmConfig } from "./types";
